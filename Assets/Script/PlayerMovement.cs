dec3e95c5be201b447d95ac80e04f393d315c947
mengyin001 <libingzham1@gmail.com>
2025-03-20 19:07:23 +0800

    v0.1
    Player walk

---
+++ Assets/Script/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; // 移动速度
+    private Rigidbody2D rb; // Rigidbody2D 组件
+    private Vector2 movement; // 存储移动输入
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>(); // 获取 Rigidbody2D 组件
+    }
+
+    void Update()
+    {
+        // 获取水平和垂直输入
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        // 移动角色
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
