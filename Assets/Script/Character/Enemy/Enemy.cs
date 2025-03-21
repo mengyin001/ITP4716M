0b20545024c5cd27860a2b922fa54fde2ab06484
mountain255 <sikshanyyds@gmail.com>
2025-03-21 23:57:38 +0800

    21/3/2025

---
+++ Assets/Script/Character/Enemy/Enemy.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("属性")]
+    [SerializeField] protected float maxHealth;
+    [SerializeField] protected float currentHealth;
+    [SerializeField] private Transform player;
+
+    [SerializeField] private float chaseDistance = 3f; // 追击距离
+    [SerializeField] private float attackDistance = 0.8f; // 攻击距离
+    [SerializeField] private float invulnerableDuration = 1f; // 无敌持续时间
+
+    private bool invulnerable;
+
+    public UnityEvent<Vector2> OnMovementInput;
+    public UnityEvent OnHurt;
+    public UnityEvent OnDie;
+    public UnityEvent OnAttack;
+
+    protected virtual void OnEnable()
+    {
+        currentHealth = maxHealth;
+    }
+
+    public virtual void TakeDamage(float damage)
+    {
+        if (invulnerable)
+            return;
+
+        if (currentHealth - damage > 0f)
+        {
+            currentHealth -= damage;
+            StartCoroutine(InvulnerableCoroutine()); // 启动无敌时间协程
+            // 执行角色受伤动画
+            OnHurt?.Invoke();
+        }
+        else
+        {
+            // 死亡
+            Die();
+        }
+    }
+
+    public virtual void Die()
+    {
+        currentHealth = 0f;
+        // 执行角色死亡动画
+        OnDie?.Invoke();
+        // 这里可以添加其他死亡处理逻辑，比如禁用敌人
+        gameObject.SetActive(false); // 假设禁用敌人
+    }
+
+    // 无敌
+    
+    protected virtual IEnumerator InvulnerableCoroutine()
+    {
+        invulnerable = true;
+        // 等待无敌时间
+        yield return new WaitForSeconds(invulnerableDuration);
+        invulnerable = false;
+    }
+
+    private void Update()
+    {
+        if (player == null)
+            return;
+
+        float distance = Vector2.Distance(player.position, transform.position);
+
+        if (distance < chaseDistance) // 小于追击距离
+        {
+            if (distance <= attackDistance) // 是否处于攻击距离
+            {
+                // 攻击玩家
+                OnMovementInput?.Invoke(Vector2.zero); // 停止移动
+                OnAttack?.Invoke();
+            }
+            else
+            {
+                // 追击玩家
+                Vector2 direction = player.position - transform.position;
+                OnMovementInput?.Invoke(direction.normalized);
+            }
+        }
+        else
+        {
+            // 放弃追击
+            OnMovementInput?.Invoke(Vector2.zero);
+        }
+    }
+}
---
+++ Assets/Script/Character/Enemy/EnemyController.cs
@@ -0,0 +1,120 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [Header("属性")]
+    [SerializeField] private float currentSpeed = 0;
+    public Vector2 MovementInput { get; set; }
+    [Header("攻击")]
+    [SerializeField] private bool isAttack = true;
+    [SerializeField] private float attackCoolDuration = 1;//冷却时间
+    /*
+    [Header("击退")]
+    [SerializeField] private bool isKnockback=true;
+    [SerializeField] private float KnockbackForce = 10f;
+    [SerializeField] private float KnockbackForceDuration = 0.1f;
+    */
+    private Rigidbody2D rb;
+    private Collider2D enemyCollider;
+    private SpriteRenderer sr;
+    private Animator anim;
+
+    private bool isHurt;
+    private bool isDie;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+       // enemyCollider = GetComponent<Collider2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void FixedUpdate()//死亡不移动
+    {
+        if( !isDie)
+            Move();
+        SetAnimation();
+    }
+    void Move()//追踪移动方向
+    {
+        if (MovementInput.magnitude > 0.1f && currentSpeed >= 0)
+        {
+            rb.linearVelocity = MovementInput * currentSpeed;
+            
+            if (MovementInput.x < 0)//左
+            {
+                sr.flipX = false;
+            }
+            if (MovementInput.x > 0)//右
+            {
+                sr.flipX = true;
+            }
+        }
+        else
+        {
+            rb.linearVelocity = Vector2.zero;
+        }
+    }
+    public void Attack()//判断发动攻击
+    {
+        if (isAttack)
+        {
+            isAttack = false;
+            StartCoroutine(nameof(AttackCoroutine));
+        }
+    }
+
+    IEnumerator AttackCoroutine()//攻击协议
+    {
+        anim.SetTrigger("isAttack");
+
+        yield return new WaitForSeconds(attackCoolDuration);
+        isAttack = true;
+    }
+
+    public void EnemyHurt()//受伤动画播放
+    {
+        //isHurt = true;
+        anim.SetTrigger("isHurt");
+    }
+    /*
+    public void Knockback(Vector3 pos)
+    {
+        //施加击退效果
+        if(!isKnockback || !isDie){
+            return;
+        }
+
+        StartCoroutine(KnockbackCoroutine(pos));
+    }
+
+    IEnumerator KnockbackCoroutine(Vector3 pos)
+    {
+        var direction = (transform.position - pos).normalized;//击退方向
+        rb.AddForce(direction * KnockbackForce, ForceMode2D.Impulse);
+        yield return new WaitForSeconds(KnockbackForceDuration);
+        isHurt = false;
+    }*/
+    public void EnemyDie()//死亡
+    {
+        rb.linearVelocity = Vector2.zero;//死亡不能动
+        isDie = true;
+        enemyCollider.enabled = false;//禁用rigidbody
+    }
+
+    void SetAnimation()//播放动画
+    {
+        anim.SetBool("isWalk", MovementInput.magnitude > 0);
+        anim.SetBool("isDie", isDie);
+    }
+
+    public void DestroyEnemy()//oject消失
+    {
+        Destroy(this.gameObject);//销毁的对象
+    }
+
+    
+}
