e62eb834cb9141288939d70c55644c6e09e1a50e
mengyin001 <libingzham1@gmail.com>
2025-04-22 00:55:59 +0800

    v3.8
    add cion and damage

---
+++ Assets/scripts/MoneyManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using TMPro;
+
+// 货币管理系统（单例模式）
+public class MoneyManager : MonoBehaviour
+{
+    public static MoneyManager Instance;
+
+    [Header("配置")]
+    public MoneyData moneyData;
+    public TMP_Text moneyText;
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+            LoadMoney();
+            UpdateUI();
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Update()
+    {
+        UpdateUI();
+    }
+
+    // 增加货币
+    public void AddMoney(int amount)
+    {
+        moneyData.itemHeld += amount;
+        UpdateUI();
+        SaveMoney();
+    }
+
+    // 更新UI显示
+    public void UpdateUI()
+    {
+        if (moneyText != null)
+        {
+            moneyText.text = $"{moneyData.itemHeld}";
+        }
+    }
+
+    // 保存数据
+    private void SaveMoney()
+    {
+        PlayerPrefs.SetInt("PlayerMoney", moneyData.itemHeld);
+    }
+
+    // 加载数据
+    private void LoadMoney()
+    {
+        if (PlayerPrefs.HasKey("PlayerMoney"))
+        {
+            moneyData.itemHeld = PlayerPrefs.GetInt("PlayerMoney");
+        }
+    }
+
+    private void OnApplicationQuit()
+    {
+        SaveMoney();
+    }
+}
