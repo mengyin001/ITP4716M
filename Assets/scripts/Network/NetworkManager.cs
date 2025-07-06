927bbb65bce60eb01b2795f1bc9efa824860eb95
mountain255 <sikshanyyds@gmail.com>
2025-07-07 01:18:41 +0800

    7/7

--- Assets/scripts/Network/NetworkManager.cs
+++ Assets/scripts/Network/NetworkManager.cs
@@ -1,6 +1,5 @@
-using UnityEngine;
+﻿using UnityEngine;
 using Photon.Pun;
-using FunkyCode.Lighting2DMaterial;
 using Photon.Realtime;
 using TMPro;
 using UnityEngine.UI;
@@ -14,6 +13,14 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     [Header("Space")]
     public Transform spacePoint;
 
+    void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
     void Start()
     {
         PhotonNetwork.ConnectUsingSettings();
@@ -22,13 +29,22 @@ public class NetworkManager : MonoBehaviourPunCallbacks
     public override void OnConnectedToMaster()
     {
         base.OnConnectedToMaster();
-        Debug.Log("Welcome");
+        Debug.Log("Welcome to Photon Server");
         PhotonNetwork.JoinOrCreateRoom("Room", new Photon.Realtime.RoomOptions() { MaxPlayers = 4 }, default);
     }
 
     public override void OnJoinedRoom()
     {
         base.OnJoinedRoom();
+        Debug.Log($"Joined room: {PhotonNetwork.CurrentRoom.Name}");
+
+        // 检查是否是第一个进入房间的玩家（即房主）
+        if (PhotonNetwork.IsMasterClient)
+        {
+            Debug.Log("You are the Master Client (Host)");
+            // 可以在这里设置房主标识或特殊权限
+        }
+
         SpawnPlayer();
     }
 
@@ -37,4 +53,10 @@ public class NetworkManager : MonoBehaviourPunCallbacks
         GameObject _player = PhotonNetwork.Instantiate(player.name, spacePoint.position, Quaternion.identity, 0);
     }
 
+    // 当房主变更时调用
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        base.OnMasterClientSwitched(newMasterClient);
+        Debug.Log($"New Master Client: {newMasterClient.NickName}");
+    }
 }
