690155f32b092f7b2a337070fb43c6a4f306b219
mountain255 <sikshanyyds@gmail.com>
2025-07-06 22:30:27 +0800

    1

--- Assets/Prefab/Enemy/Teleport/TeleportationCircle.cs
+++ Assets/Prefab/Enemy/Teleport/TeleportationCircle.cs
@@ -1,15 +1,27 @@
-using UnityEngine;
-using UnityEngine.SceneManagement;
+﻿using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
 public class TeleportationCircle : MonoBehaviour
 {
-    public string sceneName;
+    public string targetSceneName;
+    public string loadingSceneName = "LoadingScence";
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
-            SceneLoader.targetScene = sceneName;
-            SceneManager.LoadScene("LoadingScence");
+            PhotonView playerView = other.GetComponent<PhotonView>();
+
+            // 确保只有本地玩家触发，并且是主机
+            if (playerView != null && playerView.IsMine && PhotonNetwork.IsMasterClient)
+            {
+                // 保存目标场景
+                SceneLoader.targetScene = targetSceneName;
+
+                // 主机调用加载场景，所有客户端会自动同步
+                PhotonNetwork.LoadLevel(loadingSceneName);
+            }
         }
     }
 }
